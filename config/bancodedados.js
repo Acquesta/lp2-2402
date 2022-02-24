53d4b77487e5cd2f4b2364e2fe2f6fab2b732c69
Acquesta <81311825+Acquesta@users.noreply.github.com>
2022-02-24 09:28:21 -0300

    Add files via upload

---
+++ config/bancodedados.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const conexao = async()=>{
+    var bdlocal = await mongoose.connect('mongodb+srv://LorenzoAcquesta:acquesta712399@cluster0.umqxy.mongodb.net/test')
+}
+
+const modelo = mongoose.Schema({
+    rm:String,
+    nome:String,
+    turma:String,
+    idade:String
+})
+
+const alunos = mongoose.model('modelo',alunos)
+
+module.exports = {conexao,alunos}
